b5e696e0322c5dda6d6d6ffd3987f0d0d2aa6c26
nearfield02 <memristanceisfutile@gmail.com>
2015-10-01 10:54:55 -0500

    Added text overlay to image view and updated image width to fit screen

--- app/src/main/java/com/agni/sunshine/MainActivity.java
+++ app/src/main/java/com/agni/sunshine/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,17 +41,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void populateQuotesList() {
-        myQuotes.add(new Quote("https://c2.staticflickr.com/2/1196/1065036310_d826f46270_b.jpg") );
-        myQuotes.add(new Quote("https://c1.staticflickr.com/1/192/504251019_ffc94c77b5_b.jpg"));
-        myQuotes.add(new Quote("https://c1.staticflickr.com/1/17/92230866_713ae1eeef_b.jpg"));
-        myQuotes.add(new Quote("https://c1.staticflickr.com/3/2140/2422023906_c6522c014d_b.jpg") );
-        myQuotes.add(new Quote("https://c2.staticflickr.com/4/3140/2971576313_11f623b340_b.jpg"));
-        myQuotes.add(new Quote("https://c2.staticflickr.com/6/5058/5543747770_7d37c98a54_b.jpg"));
-        myQuotes.add(new Quote("https://c1.staticflickr.com/3/2870/11335297394_21f07b19d7_b.jpg") );
-        myQuotes.add(new Quote("https://c1.staticflickr.com/1/43/122094097_8175fdee9b_b.jpg"));
-        myQuotes.add(new Quote("https://c1.staticflickr.com/9/8306/7989621033_721222caf2_b.jpg"));
-
-
+        myQuotes.add(new Quote("https://c2.staticflickr.com/2/1196/1065036310_d826f46270_b.jpg",
+                "Do one thing every day that scares you. ― Eleanor Roosevelt") );
+        myQuotes.add(new Quote("https://c1.staticflickr.com/1/192/504251019_ffc94c77b5_b.jpg",
+                "We are what we pretend to be, so we must be careful about what we pretend to be.― Kurt Vonnegut, Mother Night"));
+        myQuotes.add(new Quote("https://c1.staticflickr.com/1/17/92230866_713ae1eeef_b.jpg",
+                "Sometimes you wake up. Sometimes the fall kills you. And sometimes, when you fall, you fly.― Neil Gaiman, The Sandman, Vol. 6: Fables and Reflections"));
+        myQuotes.add(new Quote("https://c1.staticflickr.com/3/2140/2422023906_c6522c014d_b.jpg",
+                "When we love, we always strive to become better than we are. When we strive to become better than we are, everything around us becomes better too.― Paulo Coelho, The Alchemist") );
+        myQuotes.add(new Quote("https://c2.staticflickr.com/4/3140/2971576313_11f623b340_b.jpg",
+                "What's meant to be will always find a way ― Trisha Yearwood"));
+        myQuotes.add(new Quote("https://c2.staticflickr.com/6/5058/5543747770_7d37c98a54_b.jpg",
+                "The flower that blooms in adversity is the rarest and most beautiful of all.― Walt Disney Company, Mulan"));
+        myQuotes.add(new Quote("https://c1.staticflickr.com/3/2870/11335297394_21f07b19d7_b.jpg",
+                "We delight in the beauty of the butterfly, but rarely admit the changes it has gone through to achieve that beauty.― Maya Angelou") );
+        myQuotes.add(new Quote("https://c1.staticflickr.com/1/43/122094097_8175fdee9b_b.jpg",
+                "You never have to change anything you got up in the middle of the night to write.― Saul Bellow"));
+        myQuotes.add(new Quote("https://c1.staticflickr.com/9/8306/7989621033_721222caf2_b.jpg",
+                "The unexamined life is not worth living.― Socrates"));
     }
 
     private void populateListView() {
@@ -81,6 +89,9 @@ public class MainActivity extends AppCompatActivity {
             networkImageView.setImageUrl(currentQuote.getUri(), mImageLoader);
             networkImageView.setDefaultImageResId(R.drawable.landscape27);
             networkImageView.setErrorImageResId(R.drawable.landscape27);
+
+            TextView quotetextview = (TextView) itemView.findViewById(R.id.textView);
+            quotetextview.setText(currentQuote.getQuotetext());
 
             return itemView;
         }
--- app/src/main/java/com/agni/sunshine/Quote.java
+++ app/src/main/java/com/agni/sunshine/Quote.java
@@ -5,12 +5,18 @@ package com.agni.sunshine;
  */
 public class Quote {
     private String uri;
+    private String quotetext;
 
-    public Quote(String uri) {
+    public Quote(String uri, String quotetext) {
         this.uri = uri;
+        this.quotetext = quotetext;
     }
 
     public String getUri() {
         return uri;
+    }
+
+    public String getQuotetext() {
+        return quotetext;
     }
 }
