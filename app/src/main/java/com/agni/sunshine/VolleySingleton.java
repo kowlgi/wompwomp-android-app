5dba6828e7d0db22136e332c1fa228e98019f018
nearfield02 <memristanceisfutile@gmail.com>
2015-10-20 09:20:54 -0500

    Added more intelligent caching strategy -- LruBitmapCache -- that allocates three screens worth of memort

--- app/src/main/java/com/agni/sunshine/VolleySingleton.java
+++ app/src/main/java/com/agni/sunshine/VolleySingleton.java
@@ -4,7 +4,6 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
 import android.graphics.Bitmap;
-import android.support.v4.util.LruCache;
 
 /**
  * Created by kowlgi on 9/30/15.
@@ -16,15 +15,8 @@ public class VolleySingleton {
 
     private VolleySingleton(){
         mRequestQueue = Volley.newRequestQueue(MyApplication.getAppContext());
-        mImageLoader = new ImageLoader(this.mRequestQueue, new ImageLoader.ImageCache() {
-            private final LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(10);
-            public void putBitmap(String url, Bitmap bitmap) {
-                mCache.put(url, bitmap);
-            }
-            public Bitmap getBitmap(String url) {
-                return mCache.get(url);
-            }
-        });
+        mImageLoader = new ImageLoader(mRequestQueue, new LruBitmapCache(
+                LruBitmapCache.getCacheSize(MyApplication.getAppContext())));
     }
 
     public static VolleySingleton getInstance(){
